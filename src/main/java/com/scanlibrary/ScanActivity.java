f9f043c99a646266456e93eb1f8b93903678eaec
jacknson <aslimani48@gmail.com>
2020-12-14 10:19:18 +0000

    first commit

---
+++ src/main/java/com/scanlibrary/ScanActivity.java
@@ -0,0 +1,193 @@
+package com.scanlibrary;
+
+import android.annotation.SuppressLint;
+import android.app.ActionBar;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.content.ComponentCallbacks2;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
+import android.widget.Toast;
+import android.widget.Toolbar;
+
+import java.io.File;
+
+/**
+ * Created by jhansi on 28/03/15.
+ */
+public class ScanActivity extends Activity implements IScanner, ComponentCallbacks2 {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.scan_layout);
+
+        //init();
+
+        File f = new File(Environment.getExternalStorageDirectory()+"/documania-capture-document.png");
+        Toast.makeText(this, "file can read = "+f.isFile(), Toast.LENGTH_LONG).show();
+
+    }
+
+    private void init() {
+        PickImageFragment fragment = new PickImageFragment();
+        Bundle bundle = new Bundle();
+        bundle.putInt(ScanConstants.OPEN_INTENT_PREFERENCE, getPreferenceContent());
+        bundle.putInt("quality", getIntent().getIntExtra("quality", 1));
+        fragment.setArguments(bundle);
+        android.app.FragmentManager fragmentManager = getFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.add(R.id.content, fragment);
+        fragmentTransaction.commit();
+    }
+    private static ProgressDialogFragment progressDialogFragment;
+
+
+    protected int getPreferenceContent() {
+        return getIntent().getIntExtra(ScanConstants.OPEN_INTENT_PREFERENCE, 0);
+    }
+
+    @Override
+    public void onBitmapSelect(Uri uri) {
+        ScanFragment fragment = new ScanFragment();
+        Bundle bundle = new Bundle();
+        bundle.putParcelable(ScanConstants.SELECTED_BITMAP, uri);
+        fragment.setArguments(bundle);
+        android.app.FragmentManager fragmentManager = getFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.add(R.id.content, fragment);
+        fragmentTransaction.addToBackStack(ScanFragment.class.toString());
+        fragmentTransaction.commit();
+    }
+    Uri myUri;
+    @Override
+    public void onScanFinish(final Uri uri) {
+        /*ResultFragment fragment = new ResultFragment();
+        Bundle bundle = new Bundle();
+        bundle.putParcelable(ScanConstants.SCANNED_RESULT, uri);
+        fragment.setArguments(bundle);
+        android.app.FragmentManager fragmentManager = getFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.add(R.id.content, fragment);
+        fragmentTransaction.addToBackStack(ResultFragment.class.toString());
+        fragmentTransaction.commit();*/
+        myUri = uri;
+        //showProgressDialog(getResources().getString(R.string.loading));
+        AsyncTask.execute(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Intent data = new Intent();
+                    Bitmap bitmap = Utils.getBitmap(ScanActivity.this,myUri);
+
+                    Bitmap magicImg = getMagicColorBitmap(bitmap);
+
+                    Uri uriFin = Utils.getUri(ScanActivity.this, magicImg);
+
+
+                    data.putExtra(ScanConstants.SCANNED_RESULT, uriFin);
+                    ScanActivity.this.setResult(Activity.RESULT_OK, data);
+
+                    System.gc();
+                    ScanActivity.this.runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            //dismissDialog();
+                            ScanActivity.this.finish();
+                        }
+                    });
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+
+    }
+
+    protected synchronized void showProgressDialog(String message) {
+        if (progressDialogFragment != null && progressDialogFragment.isVisible()) {
+            // Before creating another loading dialog, close all opened loading dialogs (if any)
+            progressDialogFragment.dismissAllowingStateLoss();
+        }
+        progressDialogFragment = null;
+        progressDialogFragment = new ProgressDialogFragment(message);
+        FragmentManager fm = getFragmentManager();
+        progressDialogFragment.show(fm, ProgressDialogFragment.class.toString());
+    }
+
+    protected synchronized void dismissDialog() {
+        progressDialogFragment.dismissAllowingStateLoss();
+    }
+
+    @Override
+    public void onTrimMemory(int level) {
+        switch (level) {
+            case ComponentCallbacks2.TRIM_MEMORY_UI_HIDDEN:
+                /*
+                   Release any UI objects that currently hold memory.
+
+                   The user interface has moved to the background.
+                */
+                break;
+            case ComponentCallbacks2.TRIM_MEMORY_RUNNING_MODERATE:
+            case ComponentCallbacks2.TRIM_MEMORY_RUNNING_LOW:
+            case ComponentCallbacks2.TRIM_MEMORY_RUNNING_CRITICAL:
+                /*
+                   Release any memory that your app doesn't need to run.
+
+                   The device is running low on memory while the app is running.
+                   The event raised indicates the severity of the memory-related event.
+                   If the event is TRIM_MEMORY_RUNNING_CRITICAL, then the system will
+                   begin killing background processes.
+                */
+                break;
+            case ComponentCallbacks2.TRIM_MEMORY_BACKGROUND:
+            case ComponentCallbacks2.TRIM_MEMORY_MODERATE:
+            case ComponentCallbacks2.TRIM_MEMORY_COMPLETE:
+                /*
+                   Release as much memory as the process can.
+
+                   The app is on the LRU list and the system is running low on memory.
+                   The event raised indicates where the app sits within the LRU list.
+                   If the event is TRIM_MEMORY_COMPLETE, the process will be one of
+                   the first to be terminated.
+                */
+                new AlertDialog.Builder(this)
+                        .setTitle(R.string.low_memory)
+                        .setMessage(R.string.low_memory_message)
+                        .create()
+                        .show();
+                break;
+            default:
+                /*
+                  Release any non-critical data structures.
+
+                  The app received an unrecognized memory level value
+                  from the system. Treat this as a generic low-memory message.
+                */
+                break;
+        }
+    }
+
+    public native Bitmap getScannedBitmap(Bitmap bitmap, float x1, float y1, float x2, float y2, float x3, float y3, float x4, float y4);
+
+    public native Bitmap getGrayBitmap(Bitmap bitmap);
+
+    public native Bitmap getMagicColorBitmap(Bitmap bitmap);
+
+    public native Bitmap getBWBitmap(Bitmap bitmap);
+
+    public native float[] getPoints(Bitmap bitmap);
+
+    static {
+        Loader.load();
+    }
+}
